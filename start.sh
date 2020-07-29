a2fd643bc900d9fb4adfe7554af3294d8be3e22e
Adam Turner <act@adamturner.dev>
2020-07-29 11:03:20 -0700

    Add user messaging to `start` script

--- start.sh
+++ start.sh
@@ -13,6 +13,20 @@ if [ ! -f './.env' ]; then
 	cp './config/.env-example' './.env'
 fi
 
-docker-compose up --detach
+CONTAINERS=wordpress
+
+echo "Starting Docker containers"
+docker-compose up --detach $CONTAINERS
 
 docker exec -ti wordpress /bin/bash -c 'chown -R www-data: /var/www/html'
+
+while [ "$READY" != "true" ]; do
+	if curl -I http://localhost:8000 2>/dev/null | grep -q -e "HTTP/1.1 200 OK" -e "HTTP:/1.1 302 Found"; then
+		READY=true
+	else
+		sleep 4
+		echo "Waiting for WP to boot..."
+	fi
+done
+
+echo "All set!"
