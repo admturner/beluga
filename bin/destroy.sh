b1500c21e7da16bfe7e2c36f804818b1d5decaeb
Adam Turner <act@adamturner.dev>
2020-08-05 11:37:45 -0700

    Simplify destroy script

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-echo -e '\nWARNING: This will destroy all of your images and containers, whether
-up or down. You cannot restore them.\n'
-while read -r -p 'Destroy all images and containers? [y/n] ' input; do
-  case "${input}" in
-    [yY])
-      echo -e 'Destroying...\n'
-      docker-compose down --volumes
-      docker rm $(docker ps -a -q)
-      docker rmi $(docker images -q) --force
-      break
-      ;;
-    [nN])
-      echo 'Exiting...'
-      exit 0
-      break
-      ;;
-    *)
-      echo 'Please select either y or n.'
-      exit 1
-      ;;
-  esac
-done
+echo -e '\nWARNING: This will destroy all images and containers, whether up or
+down. You cannot restore them.\n'
+
+read -r -p 'Remove all images and containers? [y/N] ' input
+case "${input}" in
+  [yY])
+    echo -e 'Destroying...\n'
+    docker-compose down --volumes --remove-orphans
+    docker rm $(docker ps -a -q)
+    docker rmi $(docker images -q) --force
+    ;;
+  *|[nN])
+    echo 'Exiting...'
+    exit 0
+    ;;
+esac
