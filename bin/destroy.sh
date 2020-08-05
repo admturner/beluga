432e88cf07fadc4447c00cc77d866267db0d5319
Adam Turner <act@adamturner.dev>
2020-08-05 11:59:41 -0700

    :warning: Fix shellcheck issues

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo -e '\nWARNING: This will destroy all images and containers, whether up or
-down. You cannot restore them.\n'
+echo -e '\033[38;5;9m \nWARNING: This will destroy all images and containers, whether up or
+down. You cannot restore them.\033[0m\n'
 
 read -r -p 'Remove all images and containers? [y/N] ' input
 case "${input}" in
   [yY])
     echo -e 'Destroying...\n'
     docker-compose down --volumes --remove-orphans
-    docker rm $(docker ps -a -q)
-    docker rmi $(docker images -q) --force
+    docker rm "$(docker ps -a -q)"
+    docker rmi "$(docker images -q)" --force
     ;;
-  *|[nN])
+  *)
     echo 'Exiting...'
     exit 0
     ;;
