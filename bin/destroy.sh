a7bb144c5585425237c4cb7ca0978349cbcdf151
Adam Turner <act@adamturner.dev>
2020-08-06 11:49:12 -0700

    Fix images not removed due to non-expansion

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo -e '\033[38;5;9m \nWARNING: This will destroy all images and containers, whether up or
-down. You cannot restore them.\033[0m\n'
+echo -e "${RED} \n!!! WARNING !!! This will destroy all images and
+containers, whether up or down. You cannot restore them.${RESET}\n"
 
 read -r -p 'Remove all images and containers? [y/N] ' input
 case "${input}" in
   [yY])
     echo -e 'Destroying...\n'
     docker-compose down --volumes --remove-orphans
-    docker rm "$(docker ps -a -q)"
-    docker rmi "$(docker images -q)" --force
+    # shellcheck disable=SC2046
+    docker rm $(docker ps --all --quiet)
+    # shellcheck disable=SC2046
+    docker rmi $(docker images --quiet) --force
     ;;
   *)
     echo 'Exiting...'
